1df249fc0d38a78596db02ed2fee40509dd0818c
Tux Node <tuxnode@outlook.com>
2026-06-15 08:39:55 +0800

    feat: impl FIleIO

--- client/File.go
+++ client/File.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"container/list"
-	"math"
 
 	userlib "github.com/cs161-staff/project2-userlib"
 )
@@ -40,4 +39,24 @@ func ByteToBlock(data []byte) (block *list.List) {
 		list.PushBack(fileBlock)
 	}
 	return list
+}
+
+/*
+BlockYToByte: convert Block to byte stream
+
+	block: the block list
+*/
+func BlockYToByte(block *list.List, totalSize int) (data []byte) {
+	if totalSize <= 0 || block == nil || block.Len() == 0 {
+		return []byte{}
+	}
+	ret := make([]byte, 0, totalSize)
+	for e := block.Front(); e != nil; e = e.Next() {
+		b := e.Value.(*FileBlock)
+		ret = append(ret, b.block[:]...)
+	}
+	if len(ret) > totalSize {
+		ret = ret[:totalSize]
+	}
+	return ret
 }
